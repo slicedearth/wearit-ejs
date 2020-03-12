197a4e94ca84837cc7a07cb4d8029e12bfff7d08
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-03-12 12:14:35 +1100

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -5,3 +5,36 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const configs = require('./config');
+
+// PLACES EXPRESS INTO APP VARIABLE
+const app = express();
+// SETS PORT VARIABLE TO 3000
+const port = 3000;
+// CREATE ROUTES FOR EVERYTHING IN THE PUBLIC FOLDER
+app.use(express.static('public'));
+
+app.use('/', routes());
+// SETS THE RENDER ENGINE TO EJS
+app.set('view-engine', 'ejs');
+// STORES THE ENVIRONMENT FROM CONFIG.JS IN CONFIGS VARIABLE
+const config = configs[app.get('env')];
+// ADDS WHITESPACE TO HTML IF IN DEVELOPMENT ENVIRONMENT
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// HTTP-ERRORS MIDDLEWARE
+app.use((req, res, next) => {
+  return next(httpErrors(404, 'File Not Found'));
+});
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.locals.err = app.get('env') === 'development' ? err : {};
+  res.status(status);
+  res.send(err);
+});
+// STARTS SERVER
+app.listen(port, () => console.log(`Server started on port ${port}!`));
