1e1b54bd7f8a9a39edac65c726f1dcf7012d2824
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-03-12 12:05:41 +1100

    Installed Base Packages
    Installed and imported ejs, express and http-errors. Also created config and routes files.

---
+++ server/index.js
@@ -0,0 +1,7 @@
+// IMPORTS
+const express = require('express');
+const httpErrors = require('http-errors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const configs = require('./config');
