b42475ec71cf7c8b7d5f0b204cc3e2aee04b15a3
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-04-22 23:38:24 +1000

    Unit Test #2 With Jest
    Modified content in the data files and created a second unit test for the getData function in TestimonialService.

--- server/routes/products.js
+++ server/routes/products.js
@@ -13,26 +13,24 @@ module.exports = (param) => {
   });
   router.get('/:category', async (req, res, next) => {
     try {
+      const productsList = await productService.getList();
       const promises = [];
+      promises.push(productsList);
       promises.push(productService.getList(req.params.name));
       promises.push(productService.getCategories(req.params.category));
       const result = await Promise.all(promises);
       if (!result[0]) {
         return next();
       }
-      return res.render('products', {
+      return res.render(req.params.category, {
         page: req.params.category,
         category: result[0],
         product: result[1],
+        productsList,
       });
     } catch (err) {
       return next(err);
     }
-    // return res.render(
-    //   'products',
-    //   { page: req.params.category },
-
-    // );
   });
   router.get('/:category/:name', async (req, res, next) => {
     try {
